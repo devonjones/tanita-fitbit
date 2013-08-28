0f8e37709f2c2ce66639ee4d8f4581a64b0cd4ca
Devon Jones <devon@knewton.com>
2013-08-28 10:34:37 -0600

    Added more instructions to readme, added mroe configuration

--- README.md
+++ README.md
@@ -13,6 +13,14 @@ You may need to download a different version to make it work on your version of 
 
 SqlCeCmd uses the [Microsoft Public License](LICENSE.sqlcecmd.md)
 
+### Configuration ###
+* Install cygwin, and install the cron package
+* Edit dump-tanita.conf to reflect the dirs on your computer that the scale is connected to
+* Copy dump-tanita.\* to the directory you want to run it from
+* Copy SqlCeCmd.\* to the directory contaning Healthy Edge Db.sdf (normally in My Documents/Healthy Edge/)
+* Add the cron job using dump-tanita.cron as an example
+* The script will copy the data to any location, but I have it dumping into my Dropbox so that I can process the data from a linux box after the fact.
+
 ## FitBit ##
 The second part is a set of python tools that log in to the FitBit api to log the data extracted via dump-tanita.sh
 
--- dump-tanita.sh
+++ dump-tanita.sh
@@ -2,12 +2,12 @@
 source dump-tanita.conf
 pushd $HEALTHY_EDGE_DIR
 cp "Healthy Edge Db.sdf" HealthyEdgeDb.3.5.sdf
-./SqlCeCmd.exe -d "Data Source=HealthyEdgeDb.3.5.sdf" -e upgrade
-./SqlCeCmd.exe -d "Data Source=HealthyEdgeDb.3.5.sdf" -q "select * from information_schema.tables" > tables.txt
-./SqlCeCmd.exe -d "Data Source=HealthyEdgeDb.3.5.sdf" -q "select * from information_schema.columns" > columns.txt
-./SqlCeCmd.exe -d "Data Source=HealthyEdgeDb.3.5.sdf" -q "select * from Classes" > classes.txt
-./SqlCeCmd.exe -d "Data Source=HealthyEdgeDb.3.5.sdf" -q "select * from DBInfo" > dbinfo.txt
-./SqlCeCmd.exe -d "Data Source=HealthyEdgeDb.3.5.sdf" -q "select * from Measurements" > measurements.txt
-./SqlCeCmd.exe -d "Data Source=HealthyEdgeDb.3.5.sdf" -q "select * from Users" > users.txt
+$SQLCECMD -d "Data Source=HealthyEdgeDb.3.5.sdf" -e upgrade
+$SQLCECMD -d "Data Source=HealthyEdgeDb.3.5.sdf" -q "select * from information_schema.tables" > tables.txt
+$SQLCECMD -d "Data Source=HealthyEdgeDb.3.5.sdf" -q "select * from information_schema.columns" > columns.txt
+$SQLCECMD -d "Data Source=HealthyEdgeDb.3.5.sdf" -q "select * from Classes" > classes.txt
+$SQLCECMD -d "Data Source=HealthyEdgeDb.3.5.sdf" -q "select * from DBInfo" > dbinfo.txt
+$SQLCECMD -d "Data Source=HealthyEdgeDb.3.5.sdf" -q "select * from Measurements" > measurements.txt
+$SQLCECMD -d "Data Source=HealthyEdgeDb.3.5.sdf" -q "select * from Users" > users.txt
 cp * $DROPBOX_DIR
 popd
